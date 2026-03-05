53948e09671c1583fda22f3d8efd60a14371e62c
AIDEN GOLDEN <A.GOLDEN-2022@hull.ac.uk>
2026-03-05 11:28:43 +0000

    edited home page with new header and text

--- CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
+++ CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74572682021ae65fe9ce538920eff549991c3b74")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
