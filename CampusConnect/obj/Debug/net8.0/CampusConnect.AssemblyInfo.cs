3a362bc4be3af439c9349723b94840083099402e
AIDEN GOLDEN <A.GOLDEN-2022@hull.ac.uk>
2026-03-12 11:48:15 +0000

    Added Full skeleton of UI

--- CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
+++ CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53948e09671c1583fda22f3d8efd60a14371e62c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7443a563c0d841e6324b58aa072cc8c301ea59dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
